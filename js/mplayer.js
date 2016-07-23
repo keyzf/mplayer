1dd563119de2f7c2abd15f1c0a00a0eb4798f6a8
0936zz <zz5840@qq.com>
2016-07-23 14:31:31 +0800

    优化代码逻辑

--- js/mplayer.js
+++ js/mplayer.js
@@ -1,17 +1,54 @@
 /**
  * 创建mplayer播放器
- * @param {array} songList 歌曲列表
- * @param {number} playMode 播放模式(0->列表循环,1->单曲循环,2->随机,可选,默认为0)
- * @param {number} playList 第一首播放的歌的列表(可选,默认为0)
- * @param {number} playSong 第一首播放的歌在列表中的的索引(不算basic,可选,默认为0)
- * @param {boolean} autoPlay 是否自动播放，默认true
+ * @param {object} settings 设置信息
  */
-function Mplayer(songList,playMode,playList,playSong,autoPlay) {
-	playMode = playMode !== undefined ? playMode : 0;
-	playList = playList !== undefined ? playList : 0;
-	playSong = playSong !== undefined ? playSong : 0;
-	autoPlay = autoPlay !== undefined ? autoPlay : true;
-	var cls = this;
+
+function Mplayer(settings) {
+	$this = this;
+	$this.init(settings);
+	$this.dataList();
+	$this.bindEvents();
+	$this.play($this.settings.playList,$this.settings.playSong);
+}
+
+// 格式化数据
+Mplayer.prototype.dataList = function(){
+	$this.songArr = [];
+	for (var i = 0; i < $this.settings.list.length; i++) {
+		var basicSinger = '',
+			basicImage = '';
+			$this.songArr.push([]);
+		for (var j = 0; j < $this.settings.list[i].length; j++) {
+			if ($this.settings.list[i][j].basic) {
+				// 添加列表到页面
+				var listName = $this.settings.list[i][j].name;
+				$this.doms.list.append($('<li>').html(listName));
+				basicSinger = $this.settings.list[i][j].singer || '';
+				basicImage = $this.settings.list[i][j].img || '';
+				$this.songArr[i].listName = listName;
+			} else {
+				var singerName = $this.settings.list[i][j].singer || basicSinger,
+					songImg = $this.settings.list[i][j].img || basicImage,
+					songName = $this.settings.list[i][j].name;
+				$this.songArr[i].push({
+					'singer':singerName,
+					'image':songImg,
+					'name':songName,
+					'src':$this.settings.list[i][j].src,
+					'lrc':$this.settings.list[i][j].lrc
+				});
+			}
+		}
+	}
+};
+
+// 初始化
+Mplayer.prototype.init = function(settings){
+	var playMode = settings.playMode !== undefined ? settings.playMode : 0;
+	var playList = settings.playList !== undefined ? settings.playList : 0;
+	var playSong = settings.playSong !== undefined ? settings.playSong : 0;
+	var autoPlay = settings.autoPlay !== undefined ? Boolean(settings.autoPlay) : true;
+	var $this = this;
 	var timeArr = [];
 	// 处理播放模式
 	if (playMode > 2) {
@@ -19,7 +56,16 @@ function Mplayer(songList,playMode,playList,playSong,autoPlay) {
 	} else if (playMode < 0) {
 		playMode = 0;
 	}
-	cls.doms = {
+	// 存储设置
+	$this.settings = {
+		playMode:playMode,
+		playList:playList,
+		playSong:playSong,
+		autoPlay:autoPlay,
+		list:settings.songList
+	};
+	// 初始化dom元素
+	$this.doms = {
 		lrc:$('#mplayer-lrc-wrap'),
 		listtitle:$('#mplayer-list-select h3'),
 		title:$('#mplayer-playing-name'),
@@ -33,110 +79,63 @@ function Mplayer(songList,playMode,playList,playSong,autoPlay) {
 		playbutton:$('#mplayer-control-button-play'),
 		prevbutton:$('#mplayer-control-button-prev'),
 		nextbutton:$('#mplayer-control-button-next'),
-		modebuttons:$('#mplayer-control-modebox a')
-	};
-	// 设置播放模式
-	cls.doms.modebuttons.eq(playMode).addClass('mplayer-deep-green');
-	/**
-	 * 播放指定音乐
-	 * @param  {number} list 列表id
-	 * @param  {number} song 歌曲id
-	 * @return {void}
-	 */
-	cls.play = function (list,song) {
-		var thissong = cls.songArr[list][song];
-		cls.audiodom.attr({
-			src: thissong.src,
-			'data-currentsong': song,
-			'data-currentlist':list
-		});
-		$('#mplayer-lrcbox').animate({scrollTop: 0}, 500);
-		cls.doms.cover.attr('src', thissong.image);
-		cls.doms.title.html('<h2>'+ thissong.name + '<small>'+ thissong.singer +'</small></h2>');
-		cls.changeList(list);
-		timeArr = cls.setLrc(thissong.lrc);
-		cls.audiodom.attr('data-currentLrc',  0);
-	};
-	/**
-	 * 更换显示列表
-	 * @param  {number} list 列表序号
-	 */
-	cls.changeList = function (list) {
-		cls.doms.listtitle.html(cls.songArr[list].listName);
-		cls.doms.musicList.html('');
-		for (var i = 0; i < cls.songArr[list].length; i++) {
-			cls.doms.musicList.append($('<li>').html(cls.songArr[list][i].name + ' - ' + cls.songArr[list][i].singer).addClass('mplayer-list-song'));
-		}
-		var lis = $('#mplayer-list li');
-		lis.each(function(index, el) {
-			$(el).on('click', function() {
-				cls.play(list,index);
-			});
-		});
-		if (parseInt(cls.audiodom.attr('data-currentlist')) === list) {
-			var song = cls.audiodom.attr('data-currentsong');
-			lis.eq(song).addClass('mplayer-list-song-current');
-		}
-	};
-	/**
-	 * 设置显示时间
-	 * @param {float} time 时间(s)
-	 * @param {string} ele  显示元素id
-	 */
-	cls.setTime = function (time,ele) {
-		var min = fillByZero(parseInt(time/60),2);
-		time -= min*60;
-		var second = fillByZero(Math.round(time),2);
-		$('#'+ele).html(min+':'+second);
-	};
-	/**
-	 * 设置显示歌词
-	 * @param {string} lrc 歌词字符串
-	 */
-	cls.setLrc = function (lrc) {
-		// 匹配歌词的正则表达式
-		var reg = /\[(\d{2}):(\d{2})\.(\d{2})\]([^\n\r]*)/g;
-		var lrcObj = {};
-		while (true) {
-			var result = reg.exec(lrc);
-			if (!result) {
-				break;
-			}
-			var time = Math.round((parseInt(result[1])*60 + parseInt(result[2]) + parseInt(result[3])/100)*1000);
-			lrcObj[time] = $.trim(result[4]);
-		}
-		var timeArr = [];
-		// 将歌词输入到歌词框
-		cls.doms.lrc.html('');
-		for (var ctime in lrcObj) {
-			cls.doms.lrc.append($('<li>').html(lrcObj[ctime] || " ").addClass('mplayer-lrc').attr('id', 'mplayer-lrc-'+ctime));
-			timeArr.push(parseInt(ctime));
-		}
-		return timeArr;
+		modebox:$('#mplayer-control-modebox'),
+		modebuttons:$('#mplayer-control-modebox').find('a'),
+		listSelect:$('#mplayer-list-select-ul')
 	};
 
-	cls.audiodom = $('<audio>').attr({
+	// 设置播放模式
+	$this.doms.modebuttons.eq(playMode).addClass('mplayer-deep-green');
+	// 创建audio标签
+	$this.audiodom = $('<audio></audio>').attr({
 		'preload':'preload',
 		'data-playmode':playMode,
 		'data-currentlist':playList,
 		'data-currentsong':playSong,
-		'data-currentLrc':0
-	}).bind('canplay', function () {
-		if (autoPlay) {
+		'data-currentLrc':0,
+		'data-displayList':0
+	});
+	$this.tools = {
+		rand:function (min,max) {
+			if (!max) {
+				max = min;
+				min = 0;
+			}
+			var r = 0;
+			do {
+				r = Math.round(Math.random() * max);
+			} while(r < min);
+			return r;
+		},
+		fillByZero:function (num,digit) {
+			num = String(num);
+			for (var i = num.length; i < digit; i++) {
+				num = '0' + num;
+			}
+			return num;
+		}
+	};
+};
+
+
+
+Mplayer.prototype.bindEvents = function(){
+	$this.audiodom.bind('canplay', function () {
+		if ($this.settings.autoPlay) {
 			$(this).get(0).play();
 		}
-		autoPlay = true;
-		cls.setTime($(this).get(0).duration,'mplayer-play-process-alltime');
+		$this.settings.autoPlay = true;
+		$this.setTime($(this).prop('duration'),'mplayer-play-process-alltime');
 	}).bind('play', function() {
-		cls.doms.coverBox.addClass('mplayer-cover-animate');
-		cls.doms.playbutton.html('&#xe909;');
+		$this.doms.coverBox.addClass('mplayer-cover-animate');
+		$this.doms.playbutton.html('&#xe909;');
 	}).bind('pause', function() {
-		cls.doms.coverBox.removeClass('mplayer-cover-animate');
-		cls.doms.playbutton.html('&#xe908;');
+		$this.doms.coverBox.removeClass('mplayer-cover-animate');
+		$this.doms.playbutton.html('&#xe908;');
 	}).bind('ended', function() {
-		cls.toNext();
+		$this.toNext();
 	}).bind('timeupdate', function(event) {
-		var currentTime = Math.round($(this).get(0).currentTime*1000);
+		var currentTime = Math.round($(this).prop('currentTime')*1000);
 		// 更新歌词
 		for (var i = 0; i < timeArr.length; i++) {
 			if (currentTime <= timeArr[i]) {
@@ -144,111 +143,65 @@ function Mplayer(songList,playMode,playList,playSong,autoPlay) {
 			}
 		}
 		i--;
-		if (parseInt(cls.audiodom.attr('data-currentLrc')) !== i) {
-			cls.audiodom.attr('data-currentLrc', i);
+		if (parseInt($this.audiodom.attr('data-currentLrc')) !== i) {
+			$this.audiodom.attr('data-currentLrc', i);
 			$('#mplayer-lrcbox').animate({scrollTop: i*32}, 500);
 			$('#mplayer-lrc-wrap .mplayer-lrc-current').removeClass('mplayer-lrc-current');
 			$('#mplayer-lrc-'+timeArr[i]).addClass('mplayer-lrc-current');
 		}
 		// 更新进度条
-		cls.doms.process.css('width', $(this).get(0).currentTime/$(this).get(0).duration*100+'%');
+		$this.doms.process.css('width', $(this).prop('currentTime')/$(this).prop('duration')*100+'%');
 		// 更新显示时间
-		cls.setTime($(this).get(0).currentTime,'mplayer-play-process-currenttime');
+		$this.setTime($(this).prop('currentTime'),'mplayer-play-process-currenttime');
 	});
 	// 播放按钮
-	cls.doms.playbutton.on('click', function() {
-		if (cls.audiodom.get(0).paused) {
-			cls.audiodom.get(0).play();
+	$this.doms.playbutton.on('click', function() {
+		if ($this.audiodom.get(0).paused) {
+			$this.audiodom.get(0).play();
 		} else {
-			cls.audiodom.get(0).pause();
+			$this.audiodom.get(0).pause();
 		}
 	});
-	cls.doms.prevbutton.on('click', function() {
-		var mode = parseInt(cls.audiodom.attr('data-playmode'));
-		var s = parseInt(cls.audiodom.attr('data-currentsong'));
-		var l = parseInt(cls.audiodom.attr('data-currentlist'));
+	// 上一首
+	$this.doms.prevbutton.on('click', function() {
+		var mode = parseInt($this.audiodom.attr('data-playmode'));
+		var s = parseInt($this.audiodom.attr('data-currentsong'));
+		var l = parseInt($this.audiodom.attr('data-currentlist'));
 		if (mode == 2) {
-			cls.rand(l,s);
+			$this.rand(l,s);
 		} else {
-			cls.prev(l,s);
+			$this.prev(l,s);
 		}
 	});
-	cls.doms.nextbutton.on('click', function() {
-		var mode = parseInt(cls.audiodom.attr('data-playmode'));
-		var s = parseInt(cls.audiodom.attr('data-currentsong'));
-		var l = parseInt(cls.audiodom.attr('data-currentlist'));
+	// 下一首
+	$this.doms.nextbutton.on('click', function() {
+		var mode = parseInt($this.audiodom.attr('data-playmode'));
+		var s = parseInt($this.audiodom.attr('data-currentsong'));
+		var l = parseInt($this.audiodom.attr('data-currentlist'));
 		if (mode == 2) {
-			cls.rand(l,s);
+			$this.rand(l,s);
 		} else {
-			cls.next(l,s);
+			$this.next(l,s);
 		}
 	});
-	// 切换播放模式
-	cls.doms.modebuttons.each(function(index, el) {
-		$(el).on('click', function() {
-			cls.doms.modebuttons.removeClass('mplayer-deep-green');
-			$(el).addClass('mplayer-deep-green');
-			cls.audiodom.attr('data-playmode', index);
-		});
+	// 切换模式
+	$this.doms.modebox.on('click', 'a', function() {
+		$this.doms.modebuttons.removeClass('mplayer-deep-green');
+		$(this).addClass('mplayer-deep-green');
+		$this.audiodom.attr('data-playmode', $(this).index());
 	});
-	// 上一曲
-	cls.prev = function (l,s) {
-		s--;
-		if (s < 0) {
-			s = cls.songArr[l].length-1;
-		}
-		cls.play(l,s);
-	};
-	// 下一曲
-	cls.next = function (l,s) {
-		s++;
-		if (s >= cls.songArr[l].length) {
-			s = 0;
-		}
-		cls.play(l,s);
-	};
-	// 随机播放
-	cls.rand = function (l,s) {
-		var ss = s;
-		do{
-			s = rand(cls.songArr[l].length-1);
-		} while (s === ss);
-		
-		cls.play(l,s);
-	};
-	// 单曲循环
-	cls.repeat = function (l,s) {
-		cls.play(l,s);
-	};
-	// 正常下一首
-	cls.toNext = function () {
-		var mode = parseInt(cls.audiodom.attr('data-playmode'));
-		var s = parseInt(cls.audiodom.attr('data-currentsong'));
-		var l = parseInt(cls.audiodom.attr('data-currentlist'));
-		switch (mode) {
-			case 1:
-				cls.repeat(l,s);
-				break;
-			case 2:
-				cls.rand(l,s);
-				break;
-			default:
-				cls.next(l,s);
-				break;
-		}
-	};
-	// 时间跳转
-	cls.doms.totalProcess.get(0).onclick = function (event) {
-		event = event || window.event;
-		var x = event.layerX;
-		var percent = x/400;
-		var alltime = cls.audiodom.get(0).duration;
-		cls.audiodom.get(0).currentTime = alltime * percent;
-	};
+	// 调整进度
+	$this.doms.totalProcess.click(function(event) {
+		var x = event.offsetX;
+		var percent = x/$(this).width();
+		percent = percent > 100 ? 100 : percent;
+		var alltime = $this.audiodom.prop('duration');
+		$this.audiodom.prop('currentTime', alltime * percent);
+	});
 	// 音量调节
 	$('#mplayer-control-vol-range').on('change', function() {
 		var vol = $(this).val()/100;
-		cls.audiodom.get(0).volume = vol;
+		$this.audiodom.prop('volume', vol);
 		if (vol >= 0.8) {
 			ico = '&#xe903';
 		} else if (vol >= 0.3 && vol < 0.8) {
@@ -264,63 +217,152 @@ function Mplayer(songList,playMode,playList,playSong,autoPlay) {
 	$('#mplayer-menu-switch').on('click', function() {
 		$('#mplayer-container').toggleClass('mplayer-play-list-on');
 	});
-	cls.doms.listtitle.on('click', function() {
+	// 显示列表
+	$this.doms.listtitle.on('click', function() {
 		$('#mplayer-list-select').toggleClass('mplayer-list-select-on');
 	});
-
-	// 格式化数据
-	cls.songArr = [];
-	for (var i = 0; i < songList.length; i++) {
-		var basicSinger = '',
-			basicImage = '';
-			cls.songArr.push([]);
-		for (var j = 0; j < songList[i].length; j++) {
-			if (songList[i][j].basic) {
-				// 添加列表
-				var listName = songList[i][j].name;
-				cls.doms.list.append($('<li>').html(listName));
-				basicSinger = songList[i][j].singer || '';
-				basicImage = songList[i][j].img || '';
-				cls.songArr[i].listName = listName;
-			} else {
-				var singerName = songList[i][j].singer || basicSinger,
-					songImg = songList[i][j].img || basicImage,
-					songName = songList[i][j].name;
-				cls.songArr[i].push({
-					'singer':singerName,
-					'image':songImg,
-					'name':songName,
-					'src':songList[i][j].src,
-					'lrc':songList[i][j].lrc
-				});
-			}
-		}
-	}
 	// 切换列表
-	$('#mplayer-list-select-ul li').each(function(index, el) {
-		$(el).on('click', function() {
-			cls.changeList(index);
-			$(el).parent().parent().removeClass('mplayer-list-select-on');
-		});
+	$this.doms.listSelect.on('click', 'li', function() {
+		$this.changeList($(this).index());
+		$(this).parent().parent().removeClass('mplayer-list-select-on');
 	});
-	cls.play(playList,playSong);
-}
-function fillByZero (num,digit) {
-	num = String(num);
-	for (var i = num.length; i < digit; i++) {
-		num = '0' + num;
-	}
-	return num;
-}
+	// 切换歌曲
+	$this.doms.musicList.on('click', 'li', function() {
+		console.log($(this).index());
+		$this.play(parseInt($this.audiodom.attr('data-displayList')),$(this).index());
+	});
+};
 
-function rand (min,max) {
-	if (!max) {
-		max = min;
-		min = 0;
+
+/**
+ * 播放指定音乐
+ * @param  {number} list 列表id
+ * @param  {number} song 歌曲id
+ */
+Mplayer.prototype.play = function(list,song){
+	$this = this;
+	var thissong = $this.songArr[list][song];
+	$this.audiodom.attr({
+		src: thissong.src,
+		'data-currentsong': song,
+		'data-currentlist':list
+	});
+	$('#mplayer-lrcbox').animate({scrollTop: 0}, 500);
+	$this.doms.cover.attr('src', thissong.image);
+	$this.doms.title.html('<h2>'+ thissong.name + '<small>'+ thissong.singer +'</small></h2>');
+	$this.changeList(list);
+	timeArr = $this.setLrc(thissong.lrc);
+	$this.audiodom.attr('data-currentLrc',  0);
+};
+
+
+/**
+ * 更换显示列表
+ * @param  {number} list 列表序号
+ */
+Mplayer.prototype.changeList = function (list) {
+	$this = this;
+	$this.doms.listtitle.html($this.songArr[list].listName);
+	$this.doms.musicList.html('');
+	for (var i = 0; i < $this.songArr[list].length; i++) {
+		$this.doms.musicList.append($('<li>').html($this.songArr[list][i].name + ' - ' + $this.songArr[list][i].singer).addClass('mplayer-list-song'));
 	}
-	var r = 0;
-	do {
-		r = Math.round(Math.random() * max);
-	} while(r < min);
-	return r;
-}
+	$this.audiodom.attr('data-displayList', list);
+	if (parseInt($this.audiodom.attr('data-currentlist')) === list) {
+		var song = $this.audiodom.attr('data-currentsong');
+		console.log(song);
+		$('#mplayer-list').find('li').eq(song).addClass('mplayer-list-song-current');
+	}
+};
+
+/**
+ * 设置显示时间
+ * @param {float} time 时间(s)
+ * @param {string} ele  显示元素id
+ */
+Mplayer.prototype.setTime = function (time,ele) {
+	var min = $this.tools.fillByZero(parseInt(time/60),2);
+	time -= min*60;
+	var second = $this.tools.fillByZero(Math.round(time),2);
+	$('#'+ele).html(min+':'+second);
+};
+
+/**
+ * 设置显示歌词
+ * @param {string} lrc 歌词字符串
+ */
+Mplayer.prototype.setLrc = function (lrc) {
+	$this = this;
+	// 匹配歌词的正则表达式
+	var reg = /\[(\d{2}):(\d{2})\.(\d{2})\]([^\n\r]*)/g;
+	var lrcObj = {};
+	while (true) {
+		var result = reg.exec(lrc);
+		if (!result) {
+			break;
+		}
+		var time = Math.round((parseInt(result[1])*60 + parseInt(result[2]) + parseInt(result[3])/100)*1000);
+		lrcObj[time] = $.trim(result[4]);
+	}
+	var timeArr = [];
+	// 将歌词输入到歌词框
+	$this.doms.lrc.html('');
+	for (var ctime in lrcObj) {
+		$this.doms.lrc.append($('<li>').html(lrcObj[ctime] || " ").addClass('mplayer-lrc').attr('id', 'mplayer-lrc-'+ctime));
+		timeArr.push(parseInt(ctime));
+	}
+	return timeArr;
+};
+
+// 上一曲
+Mplayer.prototype.prev = function (l,s) {
+	$this = this;
+	s--;
+	if (s < 0) {
+		s = $this.songArr[l].length-1;
+	}
+	$this.play(l,s);
+};
+// 下一曲
+Mplayer.prototype.next = function (l,s) {
+	$this = this;
+	s++;
+	if (s >= $this.songArr[l].length) {
+		s = 0;
+	}
+	$this.play(l,s);
+};
+// 随机播放
+Mplayer.prototype.rand = function (l,s) {
+	$this = this;
+	var ss = s;
+	do{
+		s = $this.tools.rand($this.songArr[l].length-1);
+	} while (s === ss);
+	
+	$this.play(l,s);
+};
+// 单曲循环
+Mplayer.prototype.repeat = function (l,s) {
+	$this = this;
+	$this.play(l,s);
+};
+// 正常下一首
+Mplayer.prototype.toNext = function () {
+	$this = this;
+	var mode = parseInt($this.audiodom.attr('data-playmode'));
+	var s = parseInt($this.audiodom.attr('data-currentsong'));
+	var l = parseInt($this.audiodom.attr('data-currentlist'));
+	switch (mode) {
+		case 1:
+			$this.repeat(l,s);
+			break;
+		case 2:
+			$this.rand(l,s);
+			break;
+		default:
+			$this.next(l,s);
+			break;
+	}
+};
+
